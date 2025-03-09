b7965e8aae8bc8356a36cb6199c7ac19e3ef43f1
Syed Muhammad Abbas <154012274+abbasbukhawri@users.noreply.github.com>
2025-03-09 15:54:56 +0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,28 @@
+// Initialize Slick Carousel
+$(document).ready(function(){
+  $('.carousel').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
+  });
+});
